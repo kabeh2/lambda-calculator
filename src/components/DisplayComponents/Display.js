cd33071d11a6847ab52f99e151ad703a23ca3de6
kabeh2 <mattcnk@hotmail.com>
2019-07-17 11:13:06 -0400

    Finished mapping data to each button component and rendering buttons

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+export const Display = () => {
+  return (
+    <div className="display">
+      {/* Display any props data here */}
+      <h1>1</h1>
+    </div>
+  );
 };
