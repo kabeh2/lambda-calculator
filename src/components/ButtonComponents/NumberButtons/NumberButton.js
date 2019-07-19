d910b74cb5b63ed40a8ecce3758d1338573f9c76
kabeh2 <mattcnk@hotmail.com>
2019-07-19 11:22:17 -0400

    Testing out different functions to fix multiple decimal issue. Left commented out

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const NumberButton = props => {
   const numberBtn = "number-btn-";
+
+  useEffect(() => console.log(props.useDisplayValue));
+
   return (
     <button
       value={props.value}
@@ -13,6 +16,31 @@ const NumberButton = props => {
             : `${props.useDisplayValue}${props.value}`
         )
       }
+      // onClick={() =>
+      //   props.setDisplayValue(() => {
+      //     const a = /\.{2,}/.test(props.useDisplayValue.toString());
+
+      //     if (!a && props.useDisplayValue === 0) {
+      //       console.log(`hello ${a}`);
+      //       return props.value;
+      //     } else {
+      //       return `${props.useDisplayValue}${props.value}`;
+      //     }
+      //   })
+      // }
+      // onClick={() =>
+      //   props.setDisplayValue(() => {
+      //     switch (props.useDisplayValue) {
+      //       case "0":
+      //         return props.value;
+      //       // case ".":
+      //       //   if (props.useDisplayValue.toString().indexOf(".") === -1)
+      //       //     return props.value;
+      //       default:
+      //         return `${props.useDisplayValue}${props.value}`;
+      //     }
+      //   })
+      // }
     >
       {props.value}
     </button>
